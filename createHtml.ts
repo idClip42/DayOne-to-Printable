4199818aede382ed0de4d5a02f4af683a14aa732
Alexander Earley <axe6801@rit.edu>
2025-05-04 12:24:36 -0400

    Left another note

--- createHtml.ts
+++ createHtml.ts
@@ -6,6 +6,7 @@ import { marked } from 'marked';
 import CONFIG from "./config.json";
 
 // TODO: We should process all images to create JPGs with a max pixel width.
+// TODO: If possible, we shouldn't break inside the *headers* of each entry, and/or the headers and first body paragraph.
 
 const dataPath = path.join(CONFIG.INPUT_DIR, CONFIG.DATA_FILE);
 const photosDir = path.join(CONFIG.INPUT_DIR, CONFIG.PHOTOS_DIR); // Directory where your images are stored
