8ad11193e696e783ea4126e15dbaa5d99ce1af01
Alexander Earley <axe6801@rit.edu>
2025-05-07 21:50:52 -0400

    Option to hide images, just show outlines

--- src/handleEntryContentImage.ts
+++ src/handleEntryContentImage.ts
@@ -6,6 +6,13 @@ import { RESIZED_IMAGES_EXT } from "./resizeImages";
 export const ImageTokenSplit = /(!\[]\(dayone-moment:\/\/.*?\))/g;
 export const ImageTokenMatch = /!\[]\(dayone-moment:\/\/(.*?)\)/;
 
+const divStyle = CONFIG.ONLY_IMAGE_BORDERS ?
+    "border-style: solid; border-color: lightgray;" :
+    "";
+const imgStyle = CONFIG.ONLY_IMAGE_BORDERS ?
+    "opacity: 0" :
+    "";
+
 const photosDir = path.join(CONFIG.OUTPUT_DIR, CONFIG.PHOTOS_DIR); // Directory where your images are stored
 
 function findPhoto(entry: DayOneEntry, id: string) {
@@ -22,8 +29,8 @@ export function CreateImageHtml(entry:DayOneEntry, photoId: string) {
         const filename = `${photo.md5}.${RESIZED_IMAGES_EXT}`;
         const srcFilePath = path.join("..", photosDir, filename);
         return `
-<div class="entry-photo">
-    <img src="${srcFilePath}" alt="Photo" />
+<div class="entry-photo" style="${divStyle}">
+    <img src="${srcFilePath}" alt="Photo" style="${imgStyle}" />
 </div>
         `.trim();
     }
