84a530f993743c5d94c122bd005d23cba67b13f5
Alexander Earley <axe6801@rit.edu>
2025-05-18 21:40:10 -0400

    TODO updates

--- index.ts
+++ index.ts
@@ -12,8 +12,15 @@ import { GetTagsListHtml, InitializeTags } from './src/organizeTags';
 // TODO: Double-check timezone crossing and lack of timezone.
 // TODO: Page numbers!
 // TODO: Once we're in a good place with this, we should look into what libraries we can bring in to augment all of this. How should one be laying out a book in HTML/JS?
-// TODO: Image processing is rotating (unrotating?) some images. (February 2, 2025, 4:50 PM) (4ef2b394fe5008ee1428f2ce2a2bdce9.jpg)
-// TODO: Block quotes still not excluding everything. (March 13, 2025, 8:43 PM)
+// TODO: Image processing is rotating (unrotating?) some images.
+//  - February 2, 2025, 4:50 PM (4ef2b394fe5008ee1428f2ce2a2bdce9.jpg)
+//  - January 1, 2025, 1:05 PM (Hannibal Lecter)
+//  - March 22, 2025, 5:43 PM (Bike)
+//  - (Check to make sure that some of these weren't just originally oriented wrong.)
+// TODO: Block quotes still not excluding everything. (March 13, 2025, 8:43 PM) (February 9, 2025, 7:43 PM) (March 2, 2025, 8:15 AM)
+// TODO: Words being broken up by hyphens needlessly. (What will fixing this do to the page count?) (January 4, 2025, 9:54 PM)
+// TODO: Single line-breaks not working right always. (January 21, 2025, 8:20 PM - is it the italics?)
+// TODO: Surrounding text with "=="s ("==6 Optional Files==") indicates it's highlighted. (February 1, 2025, 8:36 PM) (February 9, 2025, 7:53 PM)
 
 const dataPath = path.join(CONFIG.FILES.INPUT_DIR, CONFIG.FILES.DATA_FILE);
 const outputPath = path.join(CONFIG.FILES.OUTPUT_DIR, CONFIG.FILES.OUTPUT_HTML);
