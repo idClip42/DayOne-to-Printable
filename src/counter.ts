04a3372e9644e0fee19099dfac4cc96ad5cf3644
Alexander Earley <axe6801@rit.edu>
2026-02-01 17:42:53 -0500

    Set up basic counter

---
+++ src/counter.ts
@@ -0,0 +1,33 @@
+import { DayOneEntry } from "../types/DayOneEntry";
+
+type Counter = {
+    "words": number,
+    "images": number,
+    "audios": number,
+    "videos": number,
+    "pdfs": number,
+};
+
+export function CountEntryContents(entry:DayOneEntry): Counter{
+    return {
+        "words": 0,
+        "images": entry.photos?.length || 0,
+        "audios": entry.audios?.length || 0,
+        "videos": entry.videos?.length || 0,
+        "pdfs": entry.pdfAttachments?.length || 0,
+    }
+}
+
+export function SumUpEntryContents(data: Counter[]){
+    return data.reduce<Counter>((accumulator, nextVal) => {
+        for(const key in accumulator)
+            accumulator[key] += nextVal[key];
+        return accumulator;
+    }, {
+        "words": 0,
+        "images": 0,
+        "audios": 0,
+        "videos": 0,
+        "pdfs": 0,
+    })
+}
