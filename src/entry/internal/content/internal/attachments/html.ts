9c9b1586a7386fca31df93f8829f64c1ee8cb156
Alexander Earley <axe6801@rit.edu>
2026-02-07 18:29:21 -0500

    Added a todo

--- src/entry/internal/content/internal/attachments/html.ts
+++ src/entry/internal/content/internal/attachments/html.ts
@@ -3,6 +3,10 @@ import config from "./config.json";
 export function updateHtmlAttachments(htmlText: string): string {
     let resultString = htmlText.replace(
         new RegExp(`<p>${config.attachmentTag}`, "g"),
+        // TODO: If we can make it make sense,
+        // TODO: I'd really love to get any and
+        // TODO: all HTML out of the codebase
+        // TODO: and into the `templates` folder.
         "<p class='attachment-block'>"
     );
 
