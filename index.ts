d30a224fef7d428b1d887abcd5d12b18774d2480
Alexander Earley <axe6801@rit.edu>
2026-02-07 13:16:43 -0500

    Broke out page html generation from root index

--- index.ts
+++ index.ts
@@ -1,18 +1,13 @@
-// generate-journal.ts
 import fs from "fs";
 import path from "path";
 import { DayOneEntry } from "./src/types/DayOneEntry";
 import config from "./config.json";
-import { GetDateColor, GetDateColorTestHtml } from "./src/date/color";
-import { convertEntryToHTML } from "./src/entries";
 import { ResizeImages } from "./src/preprocess/resizeImages";
-import { GetTagsListHtml, InitializeTags } from "./src/tags";
+import { InitializeTags } from "./src/tags";
 import { generateCoverHtml } from "./src/cover/generateCoverHtml";
-import { GetEntriesStatsHtml } from "./src/stats";
-import { formatDate } from "./src/date/format";
-import { isSameLocalDay } from "./src/date/compare";
+import { BuildHtml } from "./src/pages";
 
-const stylesheet = fs.readFileSync(config.files.stylesheets.interior);
+const stylesheet = fs.readFileSync(config.files.stylesheets.interior, "utf8");
 
 const dataPath = path.join(
     config.files.input.directory,
@@ -29,84 +24,10 @@ if (config.content.images.runResize) await ResizeImages();
 
 const rawJson = fs.readFileSync(dataPath, "utf-8");
 const entries: DayOneEntry[] = JSON.parse(rawJson).entries;
-
 console.log(entries.length, "entries");
 
 InitializeTags(entries);
-
-const entriesHtml: string[] = [];
-for (const e in entries) {
-    const entryIndex = Number(e);
-    const entry = entries[entryIndex];
-
-    if (entryIndex % 100 === 0) {
-        const perc = entryIndex / entries.length;
-        console.log(
-            `Entries processed: ${(perc * 100).toFixed(2)}% (${new Date(entry.creationDate).toDateString()})`
-        );
-    }
-
-    if (entry.isAllDay) {
-        console.log(entry);
-        throw new Error(
-            "Hit an 'all day' entry - figure out what to do with it."
-        );
-    }
-
-    const isSameDay = (() => {
-        if (entryIndex === 0) return false;
-        const prevEntry = entries[entryIndex - 1];
-        return isSameLocalDay(
-            {
-                iso: prevEntry.creationDate,
-                timeZone: prevEntry.location?.timeZoneName,
-            },
-            {
-                iso: entry.creationDate,
-                timeZone: entry.location?.timeZoneName,
-            }
-        );
-    })();
-    if (!isSameDay) {
-        const monthColor = GetDateColor(
-            entry.creationDate,
-            entry.location?.timeZoneName,
-            0.4
-        );
-        entriesHtml.push(
-            `<div class="new-day" style="color: ${monthColor}"><span>${formatDate(entry.creationDate, entry.location?.timeZoneName, false)}</span></div>`
-        );
-    }
-
-    const entryHtml = convertEntryToHTML(entry);
-    entriesHtml.push(entryHtml);
-}
-
-const fullHTML = `
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Journal Export</title>
-
-        <style>
-            ${stylesheet}
-        </style>
-
-        <!-- Bringing in "pagedjs" package -->
-        <script src="../node_modules/pagedjs/dist/paged.polyfill.js"></script>
-    </head>
-    <body>
-        <!-- <div>${GetDateColorTestHtml()}</div> -->
-
-        ${GetEntriesStatsHtml(entries)}
-        ${GetTagsListHtml()}    
-        <div id="entries">
-            ${entriesHtml.join("\n")}
-        </div>
-    </body>
-</html>
-`.trim();
+const fullHTML = BuildHtml(entries, stylesheet);
 
 fs.writeFileSync(outputPath, fullHTML);
 console.log(`✅ Exported HTML journal to ${outputPath}`);
---
+++ src/pages/index.ts
@@ -0,0 +1,85 @@
+import { GetDateColor, GetDateColorTestHtml } from "../date/color";
+import { isSameLocalDay } from "../date/compare";
+import { formatDate } from "../date/format";
+import { convertEntryToHTML } from "../entries";
+import { GetEntriesStatsHtml } from "../stats";
+import { GetTagsListHtml } from "../tags";
+import { DayOneEntry } from "../types/DayOneEntry";
+
+export function BuildHtml(entries: DayOneEntry[], styleCss: string): string {
+    const entriesHtml: string[] = [];
+    for (const e in entries) {
+        const entryIndex = Number(e);
+        const entry = entries[entryIndex];
+
+        if (entryIndex % 100 === 0) {
+            const perc = entryIndex / entries.length;
+            console.log(
+                `Entries processed: ${(perc * 100).toFixed(2)}% (${new Date(entry.creationDate).toDateString()})`
+            );
+        }
+
+        if (entry.isAllDay) {
+            console.log(entry);
+            throw new Error(
+                "Hit an 'all day' entry - figure out what to do with it."
+            );
+        }
+
+        const isSameDay = (() => {
+            if (entryIndex === 0) return false;
+            const prevEntry = entries[entryIndex - 1];
+            return isSameLocalDay(
+                {
+                    iso: prevEntry.creationDate,
+                    timeZone: prevEntry.location?.timeZoneName,
+                },
+                {
+                    iso: entry.creationDate,
+                    timeZone: entry.location?.timeZoneName,
+                }
+            );
+        })();
+        if (!isSameDay) {
+            const monthColor = GetDateColor(
+                entry.creationDate,
+                entry.location?.timeZoneName,
+                0.4
+            );
+            entriesHtml.push(
+                `<div class="new-day" style="color: ${monthColor}"><span>${formatDate(entry.creationDate, entry.location?.timeZoneName, false)}</span></div>`
+            );
+        }
+
+        const entryHtml = convertEntryToHTML(entry);
+        entriesHtml.push(entryHtml);
+    }
+
+    const fullHTML = `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Journal Export</title>
+    
+            <style>
+                ${styleCss}
+            </style>
+    
+            <!-- Bringing in "pagedjs" package -->
+            <script src="../node_modules/pagedjs/dist/paged.polyfill.js"></script>
+        </head>
+        <body>
+            <!-- <div>${GetDateColorTestHtml()}</div> -->
+    
+            ${GetEntriesStatsHtml(entries)}
+            ${GetTagsListHtml()}    
+            <div id="entries">
+                ${entriesHtml.join("\n")}
+            </div>
+        </body>
+    </html>
+    `.trim();
+
+    return fullHTML;
+}
