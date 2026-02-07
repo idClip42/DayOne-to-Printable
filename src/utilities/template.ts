032e442c81a4f71750d4c53add99eb39530894df
Alexander Earley <axe6801@rit.edu>
2026-02-07 14:08:36 -0500

    Brought in handlebars templating

---
+++ src/utilities/template.ts
@@ -0,0 +1,15 @@
+import fs from "fs";
+import path from "path";
+import Handlebars from "handlebars";
+
+export function renderTemplate<T>(templatePath: string, data: T): string {
+    const absolutePath = path.resolve(templatePath);
+    const source = fs.readFileSync(absolutePath, "utf8");
+
+    const template = Handlebars.compile(source, {
+        noEscape: true, // important for CSS / raw text
+        strict: true,
+    });
+
+    return template(data);
+}
