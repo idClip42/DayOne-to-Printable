ea7df8279588da9e4d5585f2ab195e88600ed61e
Alexander Earley <axe6801@rit.edu>
2026-02-08 16:45:24 -0500

    Spine accent variable reduced to hue

--- src/cover/internal/cssVars.ts
+++ src/cover/internal/cssVars.ts
@@ -1,17 +1,12 @@
 import config from "../../../config.json";
 import { numberToHue } from "../../utilities/color";
 
-function getYearAccentColor(year: number): string {
+export function generateCssVars(year: number) {
+    const dimensions = config.cover.dimensions;
+
     /** cool-blue starting point */
     const YEAR_ACCENT_START = 210;
     const hue = numberToHue(year, YEAR_ACCENT_START);
-    // TODO: Move the hsl string down.
-    return `hsl(${hue}, 70%, 55%)`;
-}
-
-export function generateCssVars(year: number) {
-    const dimensions = config.cover.dimensions;
-    const accentColor = getYearAccentColor(year);
 
     return `
 :root {
@@ -19,7 +14,7 @@ export function generateCssVars(year: number) {
   --height: ${dimensions.heightIn}in;
   --spine-width: ${dimensions.spineWidthIn}in;
   --hinge-in: ${dimensions.hingeIn}in;
-  --spine-accent-color: ${accentColor};
+  --spine-accent-hue: ${hue};
 }
     `.trim();
 }
