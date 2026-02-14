954871a5214b6d505b5c2b66cdef564cafadda4a
Alexander Earley <axe6801@rit.edu>
2026-02-14 11:29:34 -0500

    Changed journal test file name format

--- testJournal.ts
+++ testJournal.ts
@@ -30,9 +30,11 @@ for (const e in entries) {
     const d = new Date(entry.creationDate);
     const name =
         d.getFullYear().toString() +
-        (d.getMonth() + 1).toString().padStart(2, "0") +
-        d.getDate().toString().padStart(2, "0") +
         "-" +
+        (d.getMonth() + 1).toString().padStart(2, "0") +
+        "-" +
+        d.getDate().toString().padStart(2, "0") +
+        "_" +
         d.getHours().toString().padStart(2, "0") +
         d.getMinutes().toString().padStart(2, "0");
 
