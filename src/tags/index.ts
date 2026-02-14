8bff2786d234a02ab5ab74487304f917e45701cd
Alexander Earley <axe6801@rit.edu>
2026-02-14 13:14:10 -0500

    Rewrote tags file to only template once per tag

--- src/tags/index.ts
+++ src/tags/index.ts
@@ -5,39 +5,56 @@ import { renderTemplate } from "../utilities/template";
 import { TagTemplateVars } from "../templates/tag.hbs";
 
 const TAG_TEMPLATE_PATH = "src/templates/tag.hbs";
+const LOREM_IPSUM_TAGS = ["Lorem", "Ipsum", "Dolor", "Sit Amet"];
+
+type SimpleTagInfo = {
+    tag: string;
+    htmlPromise: Promise<string>;
+    count: number;
+};
+
+type TagLibraryItem = Readonly<
+    ReturnType<typeof processTags>[number] & {
+        htmlPromise: Promise<string>;
+    }
+>;
 
 export class TagsLibrary {
-    private _tagsLibrary: ReturnType<typeof processTags>;
+    private readonly _tagsLibrary: ReadonlyArray<TagLibraryItem>;
 
     constructor(entries: ReadonlyArray<DayOneEntry>) {
-        this._tagsLibrary = processTags(entries);
+        this._tagsLibrary = processTags(entries)
+            .map(tagObj => {
+                const text = config.content.obfuscate
+                    ? LOREM_IPSUM_TAGS[tagObj.count % LOREM_IPSUM_TAGS.length]
+                    : tagObj.tag;
+
+                const htmlPromise = renderTemplate<TagTemplateVars>(
+                    TAG_TEMPLATE_PATH,
+                    {
+                        tag: text,
+                        hue: tagObj.hue,
+                        valuePerc: tagObj.value * 100,
+                    }
+                );
+
+                return {
+                    ...tagObj,
+                    htmlPromise,
+                };
+            })
+            .reverse(); // Reverse so highest counts are first
     }
 
     public getTagHtml(tag: string): Promise<string> {
-        const infoIndex = this._tagsLibrary.findIndex(test => test.tag === tag);
-        if (infoIndex < 0) throw new Error(`Unrecognized tag: ${tag}`);
-        const info = this._tagsLibrary[infoIndex];
-        if (!info) throw new Error(`Unrecognized tag: ${tag}`);
-
-        const LOREM_IPSUM_TAGS = ["Lorem", "Ipsum", "Dolor", "Sit Amet"];
-        const text = config.content.obfuscate
-            ? LOREM_IPSUM_TAGS[info.count % LOREM_IPSUM_TAGS.length]
-            : tag;
-
-        return renderTemplate<TagTemplateVars>(TAG_TEMPLATE_PATH, {
-            tag: text,
-            hue: info.hue,
-            valuePerc: info.value * 100,
-        });
+        return this._tagsLibrary.find(t => t.tag === tag).htmlPromise;
     }
 
-    public getOrderedTagsInfo() {
-        return this._tagsLibrary
-            .map(item => ({
-                tag: item.tag,
-                htmlPromise: this.getTagHtml(item.tag),
-                count: item.count,
-            }))
-            .reverse(); // Reverse so highest counts are first
+    public getOrderedTagsInfo(): SimpleTagInfo[] {
+        return this._tagsLibrary.map<SimpleTagInfo>(item => ({
+            tag: item.tag,
+            htmlPromise: item.htmlPromise,
+            count: item.count,
+        }));
     }
 }
