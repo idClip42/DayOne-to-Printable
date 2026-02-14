86ff3e31468cc91c8bf73b1af74b0097d161b5fe
Alexander Earley <axe6801@rit.edu>
2026-02-14 14:36:34 -0500

    Template renderer caches

--- src/utilities/template.ts
+++ src/utilities/template.ts
@@ -2,17 +2,21 @@ import fs from "fs";
 import path from "path";
 import Handlebars from "handlebars";
 
+const cache: { [key: string]: ReturnType<(typeof Handlebars)["compile"]> } = {};
+
 export async function renderTemplate<T>(
     templatePath: string,
     data: T
 ): Promise<string> {
-    const absolutePath = path.resolve(templatePath);
-    const source = await fs.promises.readFile(absolutePath, "utf8");
-
-    const template = Handlebars.compile(source, {
-        noEscape: true, // important for CSS / raw text
-        strict: true,
-    });
-
+    let template = cache[templatePath];
+    if (!template) {
+        const absolutePath = path.resolve(templatePath);
+        const source = await fs.promises.readFile(absolutePath, "utf8");
+        template = Handlebars.compile(source, {
+            noEscape: true, // important for CSS / raw text
+            strict: true,
+        });
+        cache[absolutePath] = template;
+    }
     return template(data);
 }
