1fe060206bb6fe7afea75139451fe3b101c01956
Alexander Earley <axe6801@rit.edu>
2026-02-15 23:06:17 -0500

    Redid testRun with async

--- testRun.ts
+++ testRun.ts
@@ -10,55 +10,98 @@ const OUTPUT_DIR = "output-tests";
 
 if (!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR);
 
-const testFilenames = fs.readdirSync(TEST_DIR);
-const inputFilenames = testFilenames.filter(fn => fn.endsWith(INPUT_EXT));
-const outputFilenames = testFilenames.filter(fn => fn.endsWith(OUTPUT_EXT));
+// Clear out output folder.
+// We kick this off now, and will wait
+// for it to finish later before saving
+// new output files to this folder.
+const clearOutputFolderPromise = fs.promises
+    .readdir(OUTPUT_DIR)
+    .then(preexistingDiffs =>
+        Promise.all(
+            preexistingDiffs.map(fn =>
+                fs.promises.rm(path.join(OUTPUT_DIR, fn))
+            )
+        )
+    );
 
-// Clear out output folder
-const preexistingDiffs = fs.readdirSync(OUTPUT_DIR);
-for (const diffFilename of preexistingDiffs)
-    fs.rmSync(path.join(OUTPUT_DIR, diffFilename));
+fs.promises
+    .readdir(TEST_DIR)
+    .then(testFilenames => ({
+        // We read all the test filenames
+        // and split them into input and output sets.
+        input: testFilenames.filter(fn => fn.endsWith(INPUT_EXT)),
+        output: testFilenames.filter(fn => fn.endsWith(OUTPUT_EXT)),
+    }))
+    .then(filenames =>
+        filenames.input.map(inputFilename => {
+            // We associate each input test with its
+            // matching output test.
+            const testName = inputFilename.replace(INPUT_EXT, "");
+            const outputFilename = filenames.output.find(fn =>
+                fn.startsWith(testName)
+            );
+            if (!outputFilename) {
+                console.warn(`WARN: No output found for '${inputFilename}'.`);
+                return Promise.resolve();
+            }
 
-const testNames = inputFilenames.map(fn => fn.replace(INPUT_EXT, ""));
-for (const testName of testNames) {
-    const inputFilename = inputFilenames.find(fn => fn.startsWith(testName));
-    if (!inputFilename) throw new Error(`No '${testName}' input found.`);
-    const outputFilename = outputFilenames.find(fn => fn.startsWith(testName));
-    if (!outputFilename) {
-        console.warn(`WARN: No output found for '${inputFilename}'.`);
-        continue;
-    }
+            const inputFullPath = path.join(TEST_DIR, inputFilename);
+            const expectedOutputFullPath = path.join(TEST_DIR, outputFilename);
 
-    if (!inputFilename.includes(testName))
-        throw new Error(`'${inputFilename}' doesn't have '${testName}'.`);
-    if (!outputFilename.includes(testName))
-        throw new Error(`'${outputFilename}' doesn't have '${testName}'.`);
-
-    const inputFullPath = path.join(TEST_DIR, inputFilename);
-    const inputText = fs.readFileSync(inputFullPath, "utf8");
-    const inputMarkdown = JSON.parse(inputText);
-    const expectedOutputFullPath = path.join(TEST_DIR, outputFilename);
-    const expectedOutputText = fs.readFileSync(expectedOutputFullPath, "utf8");
-
-    processText(inputMarkdown, null).then(currentOutput => {
-        if (currentOutput !== expectedOutputText) {
-            const diffFilePath = path.join(OUTPUT_DIR, `${testName}.md.diff`);
-            const targetOutputFilePath = path.join(OUTPUT_DIR, outputFilename);
-
-            const patch = createTwoFilesPatch(
-                expectedOutputFullPath, // old filename (shown in diff header)
-                targetOutputFilePath, // new filename
-                expectedOutputText,
-                currentOutput,
-                "(Expected)", // optional old header
-                "(Actual)", // optional new header
-                { context: 3 } // lines of context
+            // We kick off our input/output file
+            // loading processes.
+            const realOutputPromise = fs.promises
+                .readFile(inputFullPath, "utf8")
+                .then(inputText => processText(JSON.parse(inputText), null));
+            const expectedOutputPromise = fs.promises.readFile(
+                expectedOutputFullPath,
+                "utf8"
             );
 
-            fs.writeFileSync(diffFilePath, patch);
-            fs.writeFileSync(targetOutputFilePath, currentOutput);
+            // Then we wait for them to finish.
+            return Promise.all([expectedOutputPromise, realOutputPromise]).then(
+                ([expectedOutput, realOutput]) => {
+                    if (realOutput === expectedOutput) {
+                        return Promise.resolve();
+                    } else {
+                        // If the input and output don't match,
+                        // we create a diff and save both the
+                        // diff and the full generated output.
 
-            console.log(`FAIL: ${diffFilePath}`);
-        }
-    });
-}
+                        const diffFilePath = path.join(
+                            OUTPUT_DIR,
+                            `${testName}.md.diff`
+                        );
+                        const targetOutputFilePath = path.join(
+                            OUTPUT_DIR,
+                            outputFilename
+                        );
+
+                        const patch = createTwoFilesPatch(
+                            expectedOutputFullPath, // old filename (shown in diff header)
+                            targetOutputFilePath, // new filename
+                            expectedOutput,
+                            realOutput,
+                            "(Expected)", // optional old header
+                            "(Actual)", // optional new header
+                            { context: 3 } // lines of context
+                        );
+
+                        // We wait for the old files to be cleared out
+                        // before saving new ones.
+                        return clearOutputFolderPromise
+                            .then(() =>
+                                fs.promises.writeFile(diffFilePath, patch)
+                            )
+                            .then(() =>
+                                fs.promises.writeFile(
+                                    targetOutputFilePath,
+                                    realOutput
+                                )
+                            )
+                            .then(() => console.log(`FAIL: ${diffFilePath}`));
+                    }
+                }
+            );
+        })
+    );
