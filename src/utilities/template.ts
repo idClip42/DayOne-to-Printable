cbc9e394cf1c99a555156efffb7ec19e2ce4545b
Alexander Earley <axe6801@rit.edu>
2026-02-15 21:47:34 -0500

    Cutting execution time in half By fixing a dumb async mistake.

--- src/utilities/template.ts
+++ src/utilities/template.ts
@@ -2,21 +2,28 @@ import fs from "fs";
 import path from "path";
 import Handlebars from "handlebars";
 
-const cache: { [key: string]: ReturnType<(typeof Handlebars)["compile"]> } = {};
+type CompiledTemplate = ReturnType<(typeof Handlebars)["compile"]>;
+
+const cache: {
+    [key: string]: Promise<CompiledTemplate>;
+} = {};
 
 export async function renderTemplate<T>(
     templatePath: string,
     data: T
 ): Promise<string> {
-    let template = cache[templatePath];
-    if (!template) {
-        const absolutePath = path.resolve(templatePath);
-        const source = await fs.promises.readFile(absolutePath, "utf8");
-        template = Handlebars.compile(source, {
-            noEscape: true, // important for CSS / raw text
-            strict: true,
-        });
-        cache[absolutePath] = template;
+    let templatePromise = cache[templatePath];
+    if (!templatePromise) {
+        templatePromise = fs.promises
+            .readFile(path.resolve(templatePath), "utf8")
+            .then(source =>
+                Handlebars.compile(source, {
+                    noEscape: true, // important for CSS / raw text
+                    strict: true,
+                })
+            );
+        cache[templatePath] = templatePromise;
     }
+    const template = await templatePromise;
     return template(data);
 }
