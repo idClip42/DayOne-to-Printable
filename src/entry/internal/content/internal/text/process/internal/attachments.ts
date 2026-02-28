fe60e2a1d692b6b068f59effa3eb83238252a746
Alexander Earley <axe6801@rit.edu>
2026-02-28 16:16:05 -0500

    Revised attachments to fix code

--- src/entry/internal/content/internal/text/process/internal/attachments.ts
+++ src/entry/internal/content/internal/text/process/internal/attachments.ts
@@ -5,13 +5,18 @@ import { getAttachmentMarkdown } from "../../../attachments/textProcess";
 import { getImageFilePath } from "../../../images";
 
 const REGEX_DAY_ONE_MOMENT = /!\[]\(dayone-moment:(.*?)\)/g;
+const FIX_IMAGE_SPACING = /(?<!\n)\n(!\[]\(dayone-moment:.*?\))/g;
 
-export function fillInAttachments(
+export async function fillInAttachments(
     input: string,
     entry: DayOneEntry | null
 ): Promise<string> {
-    return replaceAsync(
-        input,
+    let output = input;
+
+    output = output.replace(FIX_IMAGE_SPACING, "\n\n$1");
+
+    output = await replaceAsync(
+        output,
         // 12: Resolve DayOne Image Attachments
         // Replaces all DayOne image links with the link
         // to the actual relevant image.
@@ -34,4 +39,6 @@ export function fillInAttachments(
             return getAttachmentMarkdown(attachmentInfo);
         }
     );
+
+    return output;
 }
